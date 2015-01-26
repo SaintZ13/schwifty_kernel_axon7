02df7f552ed6e40d5aa8fdc50b199b43a131da9d
Chris Wilson <chris@chris-wilson.co.uk>
2015-01-26 04:43:22 -0800

    Revert "drm/i915: Fix mutex->owner inspection race under DEBUG_MUTEXES"
    [ Upstream commit a50940510e94f5fb65ffd79877a60592d85598a9 ]
    
    The core fix was applied in
    
    commit a63b03e2d2477586440741677ecac45bcf28d7b1
    Author: Chris Wilson <chris@chris-wilson.co.uk>
    Date:   Tue Jan 6 10:29:35 2015 +0000
    
        mutex: Always clear owner field upon mutex_unlock()
    
    (note the absence of stable@ tag)
    
    so we can now revert our band-aid commit 226e5ae9e5f910 for -next.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Daniel Vetter <daniel.vetter@ffwll.ch>
    Cc: Jani Nikula <jani.nikula@intel.com>
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/gpu/drm/i915/i915_gem.c
+++ drivers/gpu/drm/i915/i915_gem.c
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2008-2015 Intel Corporation
+ * Copyright � 2008-2015 Intel Corporation
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
