f8829ffd516f3d4ac87db81bc95523842c882b25
Eric Dumazet <edumazet@google.com>
2016-05-23 14:24:56 -0700

    net_sched: avoid too many hrtimer_start() calls
    I found a serious performance bug in packet schedulers using hrtimers.
    
    sch_htb and sch_fq are definitely impacted by this problem.
    
    We constantly rearm high resolution timers if some packets are throttled
    in one (or more) class, and other packets are flying through qdisc on
    another (non throttled) class.
    
    hrtimer_start() does not have the mod_timer() trick of doing nothing if
    expires value does not change :
    
    	if (timer_pending(timer) &&
                timer->expires == expires)
                    return 1;
    
    This issue is particularly visible when multiple cpus can queue/dequeue
    packets on the same qdisc, as hrtimer code has to lock a remote base.
    
    I used following fix :
    
    1) Change htb to use qdisc_watchdog_schedule_ns() instead of open-coding
    it.
    
    2) Cache watchdog prior expiration. hrtimer might provide this, but I
    prefer to not rely on some hrtimer internal.
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/pkt_sched.h
+++ include/net/pkt_sched.h
@@ -60,6 +60,7 @@ psched_tdiff_bounded(psched_time_t tv1, psched_time_t tv2, psched_time_t bound)
 }
 
 struct qdisc_watchdog {
+	u64		last_expires;
 	struct hrtimer	timer;
 	struct Qdisc	*qdisc;
 };
