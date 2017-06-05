8ec4203b12ad755c2380b7712c2a427237db31f3
Andres Oportus <andresoportus@google.com>
2017-06-05 12:46:44 -0700

    ANDROID: cpufreq: stats: add uid removal for uid_time_in_state
    Bug: 62295304
    Bug: 34133340
    Test: Boot and test uid removal by writing to remove_uid_range
    
    Signed-off-by: Andres Oportus <andresoportus@google.com>
    Change-Id: Ic51fc9480716a8aad88fb55549c2b69021038a11

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -655,7 +655,7 @@ void cpufreq_task_stats_init(struct task_struct *p);
 void cpufreq_task_stats_exit(struct task_struct *p);
 void cpufreq_task_stats_remove_uids(uid_t uid_start, uid_t uid_end);
 int  proc_time_in_state_show(struct seq_file *m, struct pid_namespace *ns,
-			     struct pid *pid, struct task_struct *p);
+	struct pid *pid, struct task_struct *p);
 
 struct sched_domain;
 unsigned long cpufreq_scale_freq_capacity(struct sched_domain *sd, int cpu);
