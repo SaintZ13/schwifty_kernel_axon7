df61063d9af3bacde60fc4962cdc7f419ea57212
Joe Maples <joe@frap129.org>
2018-08-05 19:27:06 -0300

    mdss: Add state_notifier hooks
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: Henrique Pereira <pereirah2001@gmail.com>

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -38,6 +38,10 @@
 #include "mdss_dba_utils.h"
 #include "mdss_livedisplay.h"
 
+#ifdef CONFIG_STATE_NOTIFIER
+#include <linux/state_notifier.h>
+#endif
+
 #define XO_CLK_RATE	19200000
 #define CMDLINE_DSI_CTL_NUM_STRING_LEN 2
 
@@ -447,6 +451,9 @@ int mdss_dsi_panel_power_ctrl(struct mdss_panel_data *pdata,
 	switch (power_state) {
 	case MDSS_PANEL_POWER_OFF:
 		ret = mdss_dsi_panel_power_off(pdata);
+#ifdef CONFIG_STATE_NOTIFIER
+		state_suspend();
+#endif
 		break;
 	case MDSS_PANEL_POWER_ON:
 		if (mdss_dsi_is_panel_on_ulp(pdata)) {
@@ -458,6 +465,9 @@ int mdss_dsi_panel_power_ctrl(struct mdss_panel_data *pdata,
 		} else {
 			ret = mdss_dsi_panel_power_on(pdata);
 		}
+#ifdef CONFIG_STATE_NOTIFIER
+		state_resume();
+#endif
 		break;
 	case MDSS_PANEL_POWER_LP1:
 		if (mdss_dsi_is_panel_on_ulp(pdata))
