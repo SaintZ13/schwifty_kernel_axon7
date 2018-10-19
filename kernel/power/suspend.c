fb1b16030452bd9a95bb8b1ee75e3dd6c13542a2
C457 <android.c357@gmail.com>
2018-10-18 20:06:38 -0500

    PM: Revert unused ZTE changes
    These were never supposed to be merged as they
    probably depend on OEM changes in the userspace
    
    Change-Id: Ie972c0f849c3c68d46e2d6b7e6d5bb1c0ec5745f

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -474,9 +474,6 @@ static void suspend_finish(void)
  * Fail if that's not the case.  Otherwise, prepare for system suspend, make the
  * system enter the given sleep state and clean up after wakeup.
  */
-/*zte_pm add for sync*/
-extern void suspend_sys_sync_queue(void);
-
 static int enter_state(suspend_state_t state)
 {
 	int error;
@@ -500,7 +497,9 @@ static int enter_state(suspend_state_t state)
 		freeze_begin();
 
 	trace_suspend_resume(TPS("sync_filesystems"), 0, true);
-	suspend_sys_sync_queue();/*zte_pm add for sync*/
+	printk(KERN_INFO "PM: Syncing filesystems ... ");
+	sys_sync();
+	printk("done.\n");
 	trace_suspend_resume(TPS("sync_filesystems"), 0, false);
 
 	pr_debug("PM: Preparing system for %s sleep\n", pm_states[state]);
