1817cd5691582db64ab791d2b85d6682ac81a345
SaintZ13 <spaceghostpurp93@gmail.com>
2018-10-24 18:35:44 +0000

    schwifty: update build script

--- build.sh
+++ build.sh
@@ -23,14 +23,14 @@ make O="$BUILD" schwifty_axon7_defconfig
 echo "building kernel..."
 make O="$BUILD" -j30
 
-#echo "building modules..."
-#make O="$BUILD" INSTALL_MOD_PATH="." INSTALL_MOD_STRIP=1 modules_install
-#rm $BUILD/lib/modules/*/build
-#rm $BUILD/lib/modules/*/source
+echo "building modules..."
+make O="$BUILD" INSTALL_MOD_PATH="." INSTALL_MOD_STRIP=1 modules_install
+rm $BUILD/lib/modules/*/build
+rm $BUILD/lib/modules/*/source
 
-#mkdir -p $OUT/modules
-#mv "$BUILD/arch/arm64/boot/Image.gz-dtb" "$OUT/Image.gz-dtb"
-#find "$BUILD/lib/modules/" -name *.ko | xargs -n 1 -I '{}' mv {} "$OUT/modules"
+mkdir -p $OUT/modules
+mv "$BUILD/arch/arm64/boot/Image.gz-dtb" "$OUT/Image.gz-dtb"
+find "$BUILD/lib/modules/" -name *.ko | xargs -n 1 -I '{}' mv {} "$OUT/modules"
 
 echo "Image.gz-dtb can be found in $BUILD"
 
