266f6b929f8a23d2dafd2c80e916a76476d83b29
SaintZ13 <spaceghostpurp93@gmail.com>
2018-11-09 00:11:38 +0000

    schwifty: changes

--- build/include/generated/compile.h
+++ build/include/generated/compile.h
@@ -1,7 +1,7 @@
 /* This file is auto generated, version 0 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#0 SMP PREEMPT Fri Nov 9 00:02:39 UTC 2018"
+#define UTS_VERSION "#0 SMP PREEMPT Fri Nov 9 00:06:47 UTC 2018"
 #define LINUX_COMPILE_BY "SaintZ93"
 #define LINUX_COMPILE_HOST "hidden"
 #define LINUX_COMPILER "gcc version 8.2.0 (crosstool-NG crosstool-ng-1.23.0) "
